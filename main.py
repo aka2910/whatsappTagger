7a6f3c084077d4e44ee025411f95af7ba17c6c8c
Akshat Kumar <81357954+aka2910@users.noreply.github.com>
2021-11-14 01:34:42 +0530

    Update

--- main.py
+++ main.py
@@ -7,42 +7,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-
 if __name__ == '__main__':
 
     driver = webdriver.Chrome()
     driver.implicitly_wait(15)
     driver.get('https://web.whatsapp.com')
-    groupName = input("Enter name of group: ")
-    driver.find_element_by_css_selector("span[title='" + groupName + "']").click()
+    while True:
+        groupName = input("Enter name of group: ")
+        search_box = WebDriverWait(driver, 6).until(ec.presence_of_element_located(
+            (By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]')))
+        search_box.clear()
+        search_box.send_keys(groupName)
+        search_box.send_keys(Keys.ENTER)
 
-    WebDriverWait(driver, 6).until(ec.text_to_be_present_in_element
-                                   ((By.XPATH, '//*[@id="main"]/header/div[2]/div[2]/span'), ','))
-    n = len(driver.find_element_by_xpath('//*[@id="main"]/header/div[2]/div[2]/span').text.split(',')) - 1
+        WebDriverWait(driver, 6).until(ec.text_to_be_present_in_element
+                                       ((By.XPATH, '//*[@id="main"]/header/div[2]/div[2]/span'), ','))
 
-    print("participants", n + 1)
+        members = driver.find_element(By.XPATH, '//*[@id="main"]/header/div[2]/div[2]/span').text.split(', ')
 
-    inputStringAt = "@"
-    initialString = "[@everyone {}] ".format(groupName)
+        n = len(driver.find_element(By.XPATH, '//*[@id="main"]/header/div[2]/div[2]/span').text.split(', ')) - 1
 
-    inputTextElement = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    inputTextElement.send_keys(initialString)
-    inputTextElement.send_keys('\u200b' * 4000)
+        print("participants", n + 1)
+        initialHeader = f"[@everyone {groupName}]"
+        inputTextElement = WebDriverWait(driver, 6).until(ec.presence_of_element_located(
+            (By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')))
+        inputTextElement.send_keys(initialHeader)
+        inputTextElement.send_keys('\u200b' * 4000)
 
-    newLineAction = ActionChains(driver).key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
-    newLineAction.perform()
-    newLineAction.perform()
-    inputTextElement.send_keys(inputStringAt)
-
-    # driver.implicitly_wait(15)
-    # elementList = driver.find_elements_by_class_name("_2zNFv")
-    # driver.implicitly_wait(15)
-    start_time = time.time()
-    for i in range(n):
-        ActionChains(driver).send_keys(Keys.DOWN * i).send_keys(Keys.ENTER).perform()
-        if i != n - 1:
-            newLineAction.perform()
-            newLineAction.perform()
-            inputTextElement.send_keys(inputStringAt)
-        print("progress =", round((i + 1) / n * 100, 2))
-    print("time taken = {}s".format(round(time.time() - start_time, 2)))
+        ActionChains(driver).key_down(Keys.LEFT_SHIFT).send_keys(Keys.ENTER, Keys.ENTER).key_up(Keys.LEFT_SHIFT).perform()
+        inputTextElement.send_keys('@')
+        start_time = time.time()
+        for i in range(n):
+            # ActionChains(driver).send_keys(Keys.DOWN * i).send_keys(Keys.ENTER).perform()
+            inputTextElement.send_keys(members[i])
+            inputTextElement.send_keys(Keys.TAB)
+            if i != n - 1:
+                # newLineAction.perform()
+                ActionChains(driver).key_down(Keys.LEFT_SHIFT).send_keys(Keys.ENTER).key_up(
+                    Keys.LEFT_SHIFT).perform()
+                inputTextElement.send_keys('\u200F', '\u200E')
+                inputTextElement.send_keys('@')
+            print("progress =", round((i + 1) / n * 100, 2))
+        print("time taken = {}s".format(round(time.time() - start_time, 2)))
